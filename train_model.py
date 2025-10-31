cd29e4ccb7d34f70c03b62bf70c912fef5bd5baf
Dinesh Jothiram <dinesh.jothiram@fractal.ai>
2025-10-31 17:40:51 +0530

    This is third commit from Git

--- train_model.py
+++ train_model.py
@@ -22,7 +22,7 @@ r2 = r2_score(y_test, y_pred)
 mse = mean_squared_error(y_test, y_pred)
 
 print(f"Model trained successfully for first time checking and automating the pipeline")
-print(f"R2 Score: {r2:.3f}")
+print(f"R2 Score_New R2 Score: {r2:.3f}")
 print(f"MSE: {mse:.3f}")
 
 # Save model
